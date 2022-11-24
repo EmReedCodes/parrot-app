b2eedc59cd94be8775f8c8f42996b4df45b4924d
Emily Reed <Emilyreed89@gmail.com>
2022-11-23 20:50:50 -0500

    starting about page added story section

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,11 +1,25 @@
 import ContactForm from "./Contact";
-
+import "./styles/style.css"
+import { Link } from "react-router-dom";
 const About = () => {
     return (
-        <div>
+     <section>
+        
+                <span>Jump to</span>
+             
+                <article className="grid">
+                    <section>
+                        <h1>Parrot's Origin Story</h1>
+                        <p>I started Parrot for my son but quickly realized the issues he faced with speech and spelling were something many kids also struggled with. After watching him use Siri to talk to his friends and navigate the web I thought to myself what if he had a place to practice both his speech and his spelling. That was the day the idea of Parrot began to take shape.</p>
+                </section>
+                <section className="origin-bck"></section>
+                </article>
+            
+            
             <ContactForm />
-    </div>
-)
+            </section>
+         )
+
 }
 
 export default About;
--- client/src/pages/about/Contact.js
+++ client/src/pages/about/Contact.js
@@ -10,33 +10,37 @@ const ContactForm = () => {
     
 
     return ( 
-        <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Email Address
-        </label>
-        <input
-          id="email"
-          type="email" 
-          name="email"
-        />
-        <ValidationError 
-          prefix="Email" 
-          field="email"
-          errors={state.errors}
-        />
-        <textarea
-          id="message"
-          name="message"
-        />
-        <ValidationError 
-          prefix="Message" 
-          field="message"
-          errors={state.errors}
-        />
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
-      </form>
+        <article className="grid">
+          <section id="contact">
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              Email Address
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <textarea className='textBox'
+              id="message"
+              name="message"
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <button type="submit" disabled={state.submitting}>
+              Submit
+            </button>
+                  </form>
+          </section>
+        </article>
      );
 }
  
