1fa2c79d68f9af1a306711eb14535e7cb0a0ba35
Emily Reed <Emilyreed89@gmail.com>
2022-11-28 20:39:00 -0500

    changed color theme to colars, starting footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,10 +1,29 @@
 import About from "../../pages/about/About"
+import { AiFillLinkedin, AiFillTwitterCircle, AiFillGithub, AiFillContacts } from "react-icons/ai"
+import { IconContext } from "react-icons"
 
 const Footer = () => {
     
     return (
         <footer className="container-fluid">
-            footer under construction
+            
+         <section  className="holdSocialIcon">
+                
+                    <a href="#" aria-label="Twitter"><IconContext.Provider value={{ className: "socialIcon" }}>
+                        <AiFillTwitterCircle />
+                      </IconContext.Provider></a>
+                    <a href="#" aria-label="LinkedIn"><IconContext.Provider value={{ className: "socialIcon" }}>
+                        <AiFillLinkedin />
+                      </IconContext.Provider></a>
+                    <a href="#" aria-label="Github"><IconContext.Provider value={{ className: "socialIcon" }}>
+                        <AiFillGithub />
+                      </IconContext.Provider></a>
+                    <a href="#" aria-label="Portfolio/Contact"><IconContext.Provider value={{ className: "socialIcon" }}>
+                        <AiFillContacts />
+                      </IconContext.Provider></a>
+             
+         </section>
+            <h5>©Parrot 2022</h5>
         </footer>
         
       );
