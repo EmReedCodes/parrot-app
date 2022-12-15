60096f40e2a6f2db507d9d1e6c6c37d8223678a5
Emily Reed <Emilyreed89@gmail.com>
2022-12-15 18:54:11 -0500

    changing server path to deploy

--- server/server.js
+++ server/server.js
@@ -18,16 +18,23 @@ app.use(express.json())
 //in order to use body data (req.body) set to false maybe
 app.use(express.urlencoded({ extended: false })) //try both
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
-}
-
-
 //@desc sending to my routes
 app.use('/api/word', wordRoutes)
 app.use('/api/user', userRoutes)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+  
 
 
 
