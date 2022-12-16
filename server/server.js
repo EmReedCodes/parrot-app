08124884dc053684e260a0c290798fb2bf1d8727
Emily Reed <Emilyreed89@gmail.com>
2022-12-16 10:52:57 -0500

    took out uneeded images

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use('/api/user', userRoutes)
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build/index.html')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
   
     app.get('*', (req, res) =>
       res.sendFile(
