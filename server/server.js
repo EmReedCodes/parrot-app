d5c6f02748fe4fbad1a6330fd24a4d5b4cb1b8a4
Emily Reed <Emilyreed89@gmail.com>
2022-12-15 20:36:59 -0500

    adding index

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use('/api/user', userRoutes)
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build/index.html')));
   
     app.get('*', (req, res) =>
       res.sendFile(
